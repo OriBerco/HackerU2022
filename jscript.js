dc77481ed8149fd304015a6d09d50267a271d8a3
OriBerco <Oriberco@gmail.com>
2022-07-02 19:38:22 +0300

    hi

--- jscript.js
+++ jscript.js
@@ -161,3 +161,155 @@ close8.addEventListener('click',()=>{
     wrapper8.style.display = 'none';
 });
  
+
+  // Functions
+ (function(){ function buildTrivia(){
+    const output = [];
+
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+
+        const answers = [];
+
+        for(letter in currentQuestion.answers){
+
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        output.push(
+          `<div class="slideQ">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
+      }
+    );
+
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults(){
+
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    let numCorrect = 0;
+
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      else{
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+   
+  resultsContainerLive.innerHTML = `Mardi Score: ${numCorrect}`;
+  resultsContainer.innerHTML = `Your Mardi Score: ${numCorrect} out of ${myQuestions.length}`;
+  }
+  
+
+  function showSlide(n) {
+    slidesQ[currentSlideQ].classList.remove('active-slideQ');
+    slidesQ[n].classList.add('active-slideQ');
+    currentSlideQ = n;
+    if(currentSlideQ === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlideQ === slidesQ.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlideQ + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlideQ - 1);
+  }
+
+ 
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const resultsContainerLive = document.getElementById('resultsLive');
+  const submitButton = document.getElementById('submitA');
+  const myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    }
+  ];
+
+ 
+  buildTrivia();
+
+
+  const previousButton = document.getElementById("previousQ");
+  const nextButton = document.getElementById("nextQ");
+  const slidesQ = document.querySelectorAll(".slideQ");
+  let currentSlideQ = 0;
+
+  
+  showSlide(currentSlideQ);
+
+
+  submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+;
+const triviaBtn = document.querySelector('.trivia-btn');
+const secretTrivia = document.querySelector('.secret-trivia-wrapper');
+const closeTriv = document.querySelector('.closeTriv');
+triviaBtn.addEventListener('click',()=>{
+    secretTrivia.style.display = 'flex';
+    
+});
+closeTriv.addEventListener('click',()=>{
+    secretTrivia.style.display = 'none';
+});})();
